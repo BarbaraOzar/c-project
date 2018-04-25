a0bd981adbc2252d42f02190e3d6c5291964fbe6
Stefy8700isstefy8700isc <stefy8700isc@gmail.com>
2018-04-25 11:41:27 +0200

    main

--- game/game/main.c
+++ game/game/main.c
@@ -8,6 +8,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "atmega2560_drivers.h"
 #include "sequence.h"
 
@@ -20,13 +21,49 @@ int main(void)
 	
 	init_stdio(0, 10000000L);
 	sei();
-	
-	printf("I'm in the wrong main");
+
+	//create Welcome sequence
+	int i;
+	seq_t welcome = seq_create(8);
+	for(i=0; i<get_size(welcome); i++){
+		seq_add_to(welcome, i);
+	}
+
+	//create Error sequence
+	seq_t error = seq_create(8);
+	seq_add_to(error, 0);
+	seq_add_to(error, 7);
+	seq_add_to(error, 1);
+	seq_add_to(error, 6);
+	seq_add_to(error, 2);
+	seq_add_to(error, 5);
+	seq_add_to(error, 3);
+	seq_add_to(error, 4);
+		
 	while(1)
 	{
+		int level=3, input, i;
+		seq_t game_sequence = seq_create(20);
+		for (i = 0 ; i<level ; i++)
+		{ 
+			seq_add_to(game_sequence, rand() % 8);
+		}
+		
+		board_t b = board_create(PORTA, DDRA, PINB, DDRB);
+		
+		seq_display(welcome, b);
+		board_wait_for_button_press(b);
+		seq_display(game_sequence, b);
+		
+		
+		
+		
+		
+		
 		
 	}
 }
+
 #endif
 
 void ports_configuration() 
